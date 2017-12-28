7291a386f654110445cd7326c41b443c78eb3954
Wu Xiaodong <33693065+xiaodongww@users.noreply.github.com>
2017-12-28 20:30:25 +0800

    Update weightedsampler.py

--- weightedsampler.py
+++ weightedsampler.py
@@ -4,5 +4,6 @@
 batch_size = 20
 class_sample_count = [10, 1, 20, 3, 4] # dataset has 10 class-1 samples, 1 class-2 samples, etc.
 weights = 1 / torch.Tensor(class_sample_count)
+weights = weights.double()
 sampler = torch.utils.data.sampler.WeightedRandomSampler(weights, batch_size)
 trainloader = data_utils.DataLoader(train_dataset, batch_size = batch_size, shuffle=True, sampler = sampler)
