e8fba979955ee47179e1cd0c8478fbaf86d61624
Wu Xiaodong <33693065+xiaodongww@users.noreply.github.com>
2017-12-28 20:22:47 +0800

    Create weightedsampler.py

---
+++ weightedsampler.py
@@ -0,0 +1,8 @@
+# an example to use torch.utils.data.sampler.WeightedRandomSampler
+# from link: https://discuss.pytorch.org/t/how-to-prevent-overfitting/1902/5
+
+batch_size = 20
+class_sample_count = [10, 1, 20, 3, 4] # dataset has 10 class-1 samples, 1 class-2 samples, etc.
+weights = 1 / torch.Tensor(class_sample_count)
+sampler = torch.utils.data.sampler.WeightedRandomSampler(weights, batch_size)
+trainloader = data_utils.DataLoader(train_dataset, batch_size = batch_size, shuffle=True, sampler = sampler)
