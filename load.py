e254d75f51ba03ca719f91ecd53dba8ba71fb246
Wu Xiaodong <33693065+xiaodongww@users.noreply.github.com>
2018-05-01 12:51:40 +0800

    Update load.py

--- load.py
+++ load.py
@@ -1,5 +1,5 @@
 
 def load_state_dict(model, path):
-    state_dict = torch.load('my_file.pt', map_location=lambda storage, loc: storage)
+    state_dict = torch.load(path, map_location=lambda storage, loc: storage)
     model.load_state_dict(state_dict)
     return model
